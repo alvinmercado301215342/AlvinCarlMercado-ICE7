1c3e7e55acb6bb8f19d27e572197ea207984b007
Alvin Mercado <amercad8@my.centennialcollege.ca>
2022-03-20 20:18:12 -0400

    Initial Commit

---
+++ AlvinCarlMercado-ICE7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlvinCarlMercado-ICE7
+//
+//  Created by Alvin Carl Mercado on 2022-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
