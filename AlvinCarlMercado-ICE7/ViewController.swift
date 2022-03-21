ce0a99d87b7996160829ceb1a1fafa37e58b954d
Alvin Mercado <amercad8@my.centennialcollege.ca>
2022-03-20 20:36:00 -0400

    --initial commit

--- AlvinCarlMercado-ICE7/ViewController.swift
+++ AlvinCarlMercado-ICE7/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 2.0) {
+            self.myLabel.center.x += self.view.bounds.width
+            self.myTextField.center.x += self.view.bounds.width
+            self.myImageView.center.x += self.view.bounds.width
     }
-
-
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        myLabel.center.x -= view.bounds.width
+        myTextField.center.x -= view.bounds.width
+        myImageView.center.x -= view.bounds.width
+    }
 }
 
